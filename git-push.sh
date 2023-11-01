a6aaafa2a36c171eb96c53a3bb18a7be9e4ad56c
kcarronFR <kirk.carron@forgerock.com>
2023-11-01 11:38:01 -0400

    Updated README.md file again to include how to set it up locally

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /Users/kcarron/Repositories/display-debug/
 git add .
-git commit -m "Updated README.md file"
+git commit -m "Updated README.md file again to include how to set it up locally"
 git push -u origin sharedState
